04d6c6f94103fa2c77626b5ca53f1e9f00d215e0
Victoria Grinberg <31312528+ramquist@users.noreply.github.com>
2021-01-21 15:31:05 +0300

    Implement the grading mechanism

--- Bulls and Cows/task/src/bullscows/Main.java
+++ Bulls and Cows/task/src/bullscows/Main.java
@@ -1,7 +1,41 @@
 package bullscows;
 
+import java.util.Scanner;
+
 public class Main {
+    final static int secret = 1234;
+
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+        int guess = scanner.nextInt();
+        getGrade(secret, guess);
+        System.out.printf("The secret code is %d.", secret);
+    }
+
+    public static void getGrade(int secret, int guess) {
+        int bulls = 0;
+        int cows = 0;
+        for (int i = 0; i < 4; i++) {
+            int numDigit = guess % 10;
+            for (int j = 0; j < 4; j++) {
+                int secDigit = secret % 10;
+                if (numDigit == secDigit) {
+                    if (i == j) {
+                        bulls++;
+                    } else {
+                        cows++;
+                    }
+                }
+                secret = secret / 10;
+            }
+            secret = Main.secret; // restore
+            guess = guess / 10;
+        }
+
+        if (bulls != 0 || cows != 0) {
+            System.out.printf("Grade: %d bull(s) and %d cow(s)", bulls, cows);
+        } else {
+            System.out.println("Grade: None.");
+        }
     }
 }
