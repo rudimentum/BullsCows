c248477e8e4ba65be847a44918b7a3cfe9f3fb51
Victoria Grinberg <31312528+ramquist@users.noreply.github.com>
2021-01-22 22:57:03 +0300

    Create a simple random number generator
    Use System.nanoTime() for generating a secret number.

--- Bulls and Cows/task/src/bullscows/Main.java
+++ Bulls and Cows/task/src/bullscows/Main.java
@@ -3,22 +3,56 @@ package bullscows;
 import java.util.Scanner;
 
 public class Main {
-    final static int secret = 1234;
+    final static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        int secret = generateRandom();
         int guess = scanner.nextInt();
         getGrade(secret, guess);
         System.out.printf("The secret code is %d.", secret);
     }
 
+    public static int generateRandom() {
+        int length = scanner.nextInt();
+        if (length < 11 && length > 0) {
+            while (true) {
+                long pseudoRandomNumber = Long.reverse(System.nanoTime());
+                try {
+                    String temp = (pseudoRandomNumber + "").substring(0, length);
+                    if (checkUniqueDigits(temp)) {
+                        System.out.println("The random secret number is " + temp);
+                        return Integer.parseInt(temp);
+                    }
+                } catch (Exception e) {
+                }
+            }
+        } else {
+            System.out.printf("Error: can't generate a secret number with a length of %d because there aren't enough unique digits.", length);
+            return generateRandom();
+        }
+    }
+
+    public static boolean checkUniqueDigits(String str) {
+        boolean isUnique = true;
+        for (int i = 0; i < str.length(); i++) {
+            for (int j = i + 1; j < str.length(); j++) {
+                if (str.charAt(i) == str.charAt(j)) {
+                    isUnique = false;
+                    break;
+                }
+            }
+        }
+        return isUnique;
+    }
+
     public static void getGrade(int secret, int guess) {
+        int tempSecret = secret;
         int bulls = 0;
         int cows = 0;
         for (int i = 0; i < 4; i++) {
             int numDigit = guess % 10;
             for (int j = 0; j < 4; j++) {
-                int secDigit = secret % 10;
+                int secDigit = tempSecret % 10;
                 if (numDigit == secDigit) {
                     if (i == j) {
                         bulls++;
@@ -26,9 +60,9 @@ public class Main {
                         cows++;
                     }
                 }
-                secret = secret / 10;
+                tempSecret = tempSecret / 10;
             }
-            secret = Main.secret; // restore
+            tempSecret = secret; // restore
             guess = guess / 10;
         }
 
