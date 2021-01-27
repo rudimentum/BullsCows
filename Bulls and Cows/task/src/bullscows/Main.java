cbbe40ac6fd2bfc9f72e116360f09b7ffe3ffe62
Victoria Grinberg <31312528+ramquist@users.noreply.github.com>
2021-01-27 20:08:44 +0300

    Add latin chars in random generator
    Make the game more difficult by improving the code generator. Now, the secret code can contain numbers 0-9 and lowercase Latin characters a-z.

--- Bulls and Cows/task/src/bullscows/Main.java
+++ Bulls and Cows/task/src/bullscows/Main.java
@@ -8,14 +8,14 @@ public class Main {
     final static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        int secret = generateRandom();
+        String secret = getSecret();
         System.out.println("Okay, let's start a game!");
         int turn = 1;
         while (true) {
             System.out.printf("Turn %d:%n", turn);
-            int guess = scanner.nextInt();
+            String guess = scanner.next();
             int bulls = getGrade(secret, guess);
-            if (bulls == String.valueOf(secret).length()) {
+            if (bulls == secret.length()) {
                 break;
             }
             turn++;
@@ -23,33 +23,64 @@ public class Main {
         System.out.println("Congratulations! You guessed the secret code.");
     }
 
-    public static int generateRandom() {
-        Random random = new Random();
+    public static String getSecret() {
         int length = 0;
-
+        int charsRange = 0;
         while (true) {
             try {
+                System.out.println("Input the length of the secret code:");
                 length = scanner.nextInt();
+                System.out.println("Input the number of possible symbols in the code:");
+                charsRange = scanner.nextInt();
                 break;
             } catch (InputMismatchException e) {
                 System.out.println("Wrong input! Use integers from 1 to 10");
                 scanner.next();
             }
         }
-
-        if (length < 11 && length > 0) {
-            double upper = Math.pow(10, length);
-            while (true) {
-                int pseudoRandomNumber = random.nextInt((int) upper);
-                String temp = String.valueOf(pseudoRandomNumber);
-                if (temp.length() == length && checkUniqueDigits(temp)) {
-                    return Integer.parseInt(temp);
-                }
-            }
+        if (length > 36 || length < 0 || charsRange < length) {
+            System.out.printf("Error: can't generate a secret number with a length of %d because there aren't enough unique digits.%n", length);
+            return getSecret();
         } else {
-            System.out.printf("Error: can't generate a secret number with a length of %d because there aren't enough unique digits.", length);
-            return generateRandom();
+            return generateRandom(length, charsRange);
         }
+    }
+
+    public static String generateRandom(int length, int charsRange) {
+        Random random = new Random();
+        while (true) {
+            StringBuilder buffer = new StringBuilder(length);
+            for (int i = 0; i < length; i++) {
+                int n = random.nextInt(charsRange);
+                // ASCII CODE 48 - '0'
+                // ASCII CODE 97 - 'a'
+                n = n < 10 ? n + 48 : n + 87;
+                char c = (char) n;
+                buffer.append(c);
+            }
+            String generatedString = buffer.toString();
+            String secret = generatedString;
+            System.out.println(secret);
+            if (secret.length() == length && checkUniqueDigits(secret)) {
+                System.out.printf("The secret is prepared: %s %s%n", prepareAsterisks(length), prepareRanges(charsRange));
+                return secret;
+            }
+        }
+    }
+
+    public static String prepareAsterisks(int length) {
+        return "*".repeat(length);
+    }
+
+    public static String prepareRanges(int charsRange) {
+        int d = charsRange < 9 ? charsRange - 1 : 9;
+        char c;
+        if (charsRange < 10) {
+            return String.format("(0-%d)", d);
+        } else {
+            c = (char) (87 + charsRange - 1);
+        }
+        return String.format("(0-%d, a-%c)", d, c);
     }
 
     public static boolean checkUniqueDigits(String str) {
@@ -65,26 +96,19 @@ public class Main {
         return isUnique;
     }
 
-    public static int getGrade(int secret, int guess) {
-        int length = String.valueOf(secret).length();
-        int tempSecret = secret;
+    public static int getGrade(String secret, String guess) {
         int bulls = 0;
         int cows = 0;
-        for (int i = 0; i < length; i++) {
-            int numDigit = guess % 10;
-            for (int j = 0; j < length; j++) {
-                int secDigit = tempSecret % 10;
-                if (numDigit == secDigit) {
+        for (int i = 0; i < secret.length(); i++) {
+            for (int j = 0; j < guess.length(); j++) {
+                if (secret.charAt(i) == guess.charAt(j)) {
                     if (i == j) {
                         bulls++;
                     } else {
                         cows++;
                     }
                 }
-                tempSecret = tempSecret / 10;
             }
-            tempSecret = secret; // restore
-            guess = guess / 10;
         }
 
         if (bulls != 0 || cows != 0) {
